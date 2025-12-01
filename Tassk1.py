837e9bf084c23ec21f13fa9f19621dc7eefd2dbe
Sandeep mandal <kumarsandeepmandal2007@gmail.com>
2025-12-01 11:37:01 +0530

    Add files via upload

---
+++ Tassk1.py
@@ -0,0 +1,9 @@
+def factorial(n):
+    fact = 1
+    for i in range(1, n + 1):
+        fact *= i
+    return fact
+
+number = int(input("Enter a number: "))
+result = factorial(number)
+print("Factorial of", number, "is", result)
---
+++ Tassk2.py
@@ -0,0 +1,14 @@
+import math
+
+# Taking user input
+num = float(input("Enter a number: "))
+
+# Calculations using math module
+square_root = math.sqrt(num)
+natural_log = math.log(num)
+sine_value = math.sin(num)
+
+# Displaying results
+print("Square root:", square_root)
+print("Logarithm:", natural_log)
+print("Sine:", sine_value)
